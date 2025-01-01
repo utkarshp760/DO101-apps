4d0170ef016a8b169585178341b792365d11c29e
Utkarsh <utkarshpatole760@gmail.com>
2025-01-01 11:37:15 +0530

    updated app to v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 app = express();
+var response;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+    response = 'This is version 2 of the app.' + '\n';
+
+    //send the response to the client
+    res.send(response);
 });
 
 app.listen(8080, function () {
